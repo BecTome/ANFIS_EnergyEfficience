3966a3b980d3ec1b3ac6e7a752f399983a49178c
Alberto Becerra <alberto.becerra.tome@gmail.com>
2024-01-21 16:58:55 +0100

    train anfis

--- train_anfis.m
+++ train_anfis.m
@@ -3,45 +3,53 @@ clear;
 clc;
 close all;
 
-
 data = readtable("data/ENB2012_data.xlsx");
 
-% Load your dataset
+%% Load your dataset
 % Assuming X is an Nx8 matrix (N samples, 8 inputs) and Y is an Nx2 matrix (N samples, 2 outputs)
+X_orig = table2array(data(:, 1:end-2));
+Y_orig = table2array(data(:, end-1));
 
-X = table2array(data(1:10, 1:3));
-Y = table2array(data(1:10, end-1:end));
+%% Train - Test - Validation split
+%rng(123); % Set the seed for reproducibility
+n = size(X_orig, 1); % Total number of samples
+shuffledIndices = randperm(n); % Shuffle indices
+X = X_orig(shuffledIndices(:, 1:50), :); % Shuffle X
+Y = Y_orig(shuffledIndices(:, 1:50), :); % Shuffle Y
 
+
+%% Hyperparameter tuning
 % Define the range of hyperparameters to be tuned
-numMFs = [2, 4];  % Number of Membership Functions (example values)
-trainingEpochs = [3, 3];  % Number of Training Epochs (example values)
+numMFs = [2, 3];  % Number of Membership Functions (example values)
+
+trainingEpochs = 2;  % Number of Training Epochs (example values)
 
 % Initialize an array to store the results
 results = [];
 
 % Loop over the range of hyperparameters
 for i = 1:length(numMFs)
-    disp(i)
-    for j = 1:length(trainingEpochs)
+    fprintf("\nNumber of MFs:", i)
+    %for j = 1:length(trainingEpochs)
         % Generate FIS with given number of membership functions
-        opt = genfisOptions('GridPartition', ...
-                            'NumMembershipFunctions',numMFs(i), ...
-                            'InputMembershipFunctionType', 'gbellmf');
-        fis = genfis(X, Y, opt);
+    opt = genfisOptions('GridPartition', ...
+                        'NumMembershipFunctions',numMFs(i), ...
+                        'InputMembershipFunctionType', 'gaussmf');
+    fis = genfis(X, Y, opt);
 
-        % Setup training options
-        %anfisOptions = anfisOptions( 'EpochNumber', trainingEpochs(j));
-        
-        % Train the ANFIS model
-        % Pass X and Y separately to anfis when using anfisOptions
-        [trainedFis, trainError] = anfis([X, Y], fis, trainingEpochs(j));
+    % Setup training options
+    %anfisOptions = anfisOptions( 'EpochNumber', trainingEpochs(j));
+    
+    % Train the ANFIS model
+    % Pass X and Y separately to anfis when using anfisOptions
+    [trainedFis, trainError] = anfis([X, Y], fis, trainingEpochs);
 
-        % Evaluate the performance
-        finalError = trainError(end);
+    % Evaluate the performance
+    finalError = trainError(end);
 
-        % Store the results
-        results = [results; numMFs(i), trainingEpochs(j), finalError];
-    end
+    % Store the results
+    results = [results; numMFs(i), trainingEpochs, finalError];
+    %end
 end
 
 % Display the results
